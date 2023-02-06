5220ee4012fcea7e2b94d4158c060d3c0f622838
Whale <mohseninikita9@gmail.com>
2023-02-06 17:10:56 +0200

    Add project files.

---
+++ 06.02.23_Lab/City.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._02._23_Lab
+{
+    public class City
+    {
+        private string name;
+        private string country;
+        private int count;
+        private string key;
+        private string areas;
+
+        public City() { }
+        public City(string n, string c, int co, string k, string ar)
+        {
+            name = n;
+            country = c;
+            count = co;
+            key = k;
+            areas = ar;
+        }
+        public string Name { get { return name; } set { name = value; } }
+        public string Country { get { return country; } set { country = value; } }
+        public int Count { get { return count; } set { count = value; } }
+        public string Key { get { return key; } set { key = value; } }
+        public string Areas { get { return areas; } set { areas = value; } }
+
+        public void Print()
+        {
+            Console.WriteLine($"City: {name}\nCountry: {country}\nPeoples: {count}\nNum key: {key}\nAreas: {areas}");
+        }
+        public override string ToString()
+        {
+            return $"City: {name}\nCountry: {country}\nPeoples: {count}\nNum key: {key}\nAreas: {areas}";
+        }
+
+    }
+}
---
+++ 06.02.23_Lab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _06._02._23_Lab;
+
+namespace _06._02._23_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            City a = new City("Odessa", "Ukraine", 1000000, "+380", "Primorskiy, Kievskiy, Malinovskiy, Suvorivskiy") ;
+            Console.WriteLine(a);
+            Console.WriteLine();
+            a.Name = "Одесса";
+            a.Country = "Украина";
+            a.Count = 1015826;
+            a.Key = "+38048";
+            a.Areas = "Приморский, Киевский, Малиновский, Суворовский";
+            a.Print();
+        }
+    }
+}
